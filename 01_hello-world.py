c641032d982d522a4ee13cfd0f79521d08f67ca1
richysyrc <53660263+richysyrc@users.noreply.github.com>
2019-08-03 13:01:17 -0500

    Delete 01_hello-world.py

--- 01_hello-world.py
+++
@@ -1,9 +0,0 @@
-Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 22:20:52) [MSC v.1916 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-================ RESTART: C:/Users/Laboratorio/Desktop/uuu.py ================
-Hello WOrld
->>> 
-========== RESTART: C:/Users/Laboratorio/Desktop/01_hello-world.py ==========
-Hello WOrld
->>> 
